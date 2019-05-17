860f4563949c073412b50b8b7ecff85227fcf02c
张培栋 <zhangpeidong@maxtropy.com>
2019-05-17 21:15:22 +0800

    init

---
+++ config/setupProxy.js
@@ -0,0 +1,6 @@
+const proxy = require('http-proxy-middleware');
+const backendTarget = "http://10.9.7.108";
+
+module.exports = function (app) {
+  app.use(proxy('/api', { target: backendTarget, changeOrigin: true }));
+};
---
+++ src/Table/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface TableProps {
+  columns: any[],
+  dataSource: any[],
+  pagination: object,
+  rowKey: string,
+};
+
+const Table: React.FunctionComponent = (props) => {
+  return <div>
+
+  </div>
+}
